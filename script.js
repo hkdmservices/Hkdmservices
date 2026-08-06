bb38b0cbfa3b5c774f3a4870cd0a99b20fcbd4cb
hkdmservices <admin.hkdm@gmail.com>
2026-08-06 08:17:55 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// ================================
+// HKDMServices JavaScript
+// Version 1.0
+// ================================
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("HKDMServices Loaded Successfully");
+});
