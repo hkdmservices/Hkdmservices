98bb61458821a4a10af0561972dd072dc4ba39e3
hkdmservices <admin.hkdm@gmail.com>
2026-08-06 20:44:02 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,22 +3,34 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 
 import {
-  getAuth
+    getAuth
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+import {
+    getDatabase
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyADhpdfM0GaMJIkeQw7Q6eBK3u9CaWUC9k",
-  authDomain: "hkdmservices-7d59f.firebaseapp.com",
-  projectId: "hkdmservices-7d59f",
-  storageBucket: "hkdmservices-7d59f.firebasestorage.app",
-  messagingSenderId: "839538334772",
-  appId: "1:839538334772:web:7d8785f87363b6e5d8fe61"
-};
 
+    apiKey: "AIzaSyADhpdfM0GaMJIkeQw7Q6eBK3u9CaWUC9k",
+
+    authDomain: "hkdmservices-7d59f.firebaseapp.com",
+
+    projectId: "hkdmservices-7d59f",
+
+    storageBucket: "hkdmservices-7d59f.firebasestorage.app",
+
+    messagingSenderId: "839538334772",
+
+    appId: "1:839538334772:web:7d8785f87363b6e5d8fe61"
+
+};
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
-export { auth };
+const database = getDatabase(app);
+
+export { auth, database };
